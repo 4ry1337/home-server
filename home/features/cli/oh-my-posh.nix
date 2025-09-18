6dd988a50585345cef071df678667a446fdfcb92
Rakhat Yskak <yskak.rakhat@gmail.com>
2025-09-18 23:41:25 +0500

    [fix] enable zsh integration

--- home/features/cli/oh-my-posh.nix
+++ home/features/cli/oh-my-posh.nix
@@ -4,6 +4,11 @@ let cfg = config.features.cli.oh-my-posh;
 in {
   options.features.cli.oh-my-posh.enable = mkEnableOption "Enable Oh-My-Posh";
 
-  config = mkIf cfg.enable { programs.oh-my-posh = { enable = true; }; };
+  config = mkIf cfg.enable {
+    programs.oh-my-posh = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+  };
 }
 
