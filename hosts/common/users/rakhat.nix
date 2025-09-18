1eea6c6fa6529b8f0347556216a0abc8820a4287
Rakhat Yskak <yskak.rakhat@gmail.com>
2025-09-18 05:07:44 +0500

    [home] leaked rsa

--- hosts/common/users/rakhat.nix
+++ hosts/common/users/rakhat.nix
@@ -21,7 +21,7 @@
       "qemu-libvirtd"
     ];
     openssh.authorizedKeys.keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDZlNyr14Ak7PV0y6GOVBaWH5IcWing9PvjZVEqtJDnt91lj9UZehOeIKpE/PMv2lmhf6LFGoUQtOD7RGBhFSfZEdsGtEso2p4f7QKqwNS6C1wbDaoCUCYXVyhi2EwRA5anWLaCHfcMzkp/zk/DCDIJY2OQhvuE37HL2VxYrtEXQw3XiNXB1UMGETQsz6Ess/oGsdO0qXOGt66c6hdwTAWr1mFqr+QMuQ3cTQibOO5AciwKLVAPg2nJEzRImQhNu/q2hOT4/Bpp00R+dqnik/hRHecsETxj9vy3i9ktibwYg8f3hXXCPIH8RUxgfia7AzvRHME21/MO1MywGLEM+AftZzBBlT6wPBOPswasUje1HvyVdC7KAkZCm34gQ0FG8JeNuRghyd2+5Ts8NNea2qQz7wE3oB+NBv3VTPqUo8+285Zf5ESx3owhatQtEBMZnlJflGrEThx9jKVR7fSj9F2459HyttydpevvwQDZn1OsS6xCKp+lNuHCiRY6KanjTUE= rakhat@blind-warrior"
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDVduyDowbgTbA7h8uKunUTZZhDsqPGBgkkAL8uOiMx6tcxxLqSDJ+ZjLky/zAh+LDyW8KfrBEASOrH9UfiGuaeq/9sMqZFHuQY6hjE3r5uJlO8a3VyIoOh9eoZswZgzELk1GBU3SF3yq028SPD5xrhV4xp8Hq/lFLooNTUMi3hzSDKEB05SWaXu89D3RW9bO1A++ByBp93l6JXTpOItZX3+vraEjgRiLUXWx6CGlAIgCWhmSv+i+C5k4wkWU65PLSxKzVXJEyHQ62TWf1tzyInMuMz7r3TX+YCMYeDOSR946tpKcx4rJJ8vwzptS3zIhCmTFy5xvHrY5bD75CeRTj4K7ev3qwLrAffeWlCnB+VrI50z/QGnHG0PHU4N2wndUkwqhCv3/Ev2Wxwe46AQ1Rkro9cFkJ0uSiKxJxKFyo82Xq9iWs4z0EXic6WndAEOYXhW9e8m1jCLdrQRKg+KhC9B14qJFUD6ZNZUNcFJ7BFCqaQE9hsdmAXTPSElnoFf6M= rakhat@blind-warrior"
     ];
     packages = [inputs.home-manager.packages.${pkgs.system}.default];
   };
