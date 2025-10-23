f0132d75b51eb2d01d2f30974b89a5bc8b29768b
rakhat <yskak.rakhat@gmail.com>
2025-10-23 18:05:10 +0500

    [host][blind-warrior] add iw

--- home/features/cli/default.nix
+++ home/features/cli/default.nix
@@ -44,6 +44,7 @@
     aria2 # A lightweight multi-protocol & multi-source command-line download utility
     socat # replacement of openbsd-netcat
     nmap # A utility for network discovery and security auditing
+    iw
     ipcalc # it is a calculator for the IPv4/v6 addresses
   ];
 }
