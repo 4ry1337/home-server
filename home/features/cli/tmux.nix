3b31d69b776e14e3ed50f9b3eea7346409606ecd
rakhat <yskak.rakhat@gmail.com>
2026-05-19 15:39:10 +0500

    fix: set rtpFilePath for tokyo-night tmux plugin to match actual filename

--- home/features/cli/tmux.nix
+++ home/features/cli/tmux.nix
@@ -4,6 +4,7 @@ let
   cfg = config.features.cli.tmux;
   tokyo-night = pkgs.tmuxPlugins.mkTmuxPlugin {
     pluginName = "tokyo-night";
+    rtpFilePath = "tokyo-night.tmux";
     version = "unstable-2023-01-06";
     src = pkgs.fetchFromGitHub {
       owner = "janoamaral";
@@ -49,8 +50,8 @@ in {
         unbind c
         unbind '"'
 
-        bind n split-window -v -c "#{pane_current_path}"
-        bind s split-window -h -c "#{pane_current_path}"
+        bind v split-window -v -c "#{pane_current_path}"
+        bind b split-window -h -c "#{pane_current_path}"
         bind a new-window -c "#{pane_current_path}"
 
         set -g prefix C-Space
@@ -79,10 +80,9 @@ in {
         bind -n M-h previous-window
         bind -n M-l next-window
 
-        # set -g @tokyo-night-tmux_window_id_style none
-        set -g @tokyo-night-tmux_window_id_style hsquare
         set -g @tokyo-night-tmux_theme storm    # storm | day | default to 'night'
         set -g @tokyo-night-tmux_transparent 1  # 1 or 0
+        set -g @tokyo-night-tmux_window_id_style none
         set -g @tokyo-night-tmux_window_tidy_icons 1
         set -g @tokyo-night-tmux_show_datetime 0
         set -g @tokyo-night-tmux_show_git 0
