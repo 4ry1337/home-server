8c970d6d8b09e600a2b1f23664d55c2d6ffbbcb8
rakhat <yskak.rakhat@gmail.com>
2026-06-26 17:27:35 +0500

    feat: update locales

--- hosts/modules/time_and_lang.nix
+++ hosts/modules/time_and_lang.nix
@@ -1,26 +1,18 @@
 { ... }:
-let
-  timezone = "Asia/Almaty";
-  locale = "en_US.UTF-8";
-  locale_extra = "ru_RU.UTF-8";
-in
 {
-  time.timeZone = timezone;
-  i18n.defaultLocale = locale;
+  time.timeZone = "Asia/Almaty";
+  i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocales = [
     "en_US.UTF-8/UTF-8"
     "ru_RU.UTF-8/UTF-8"
+    "kk_KZ.UTF-8/UTF-8"
   ];
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = locale_extra;
-    LC_IDENTIFICATION = locale_extra;
-    LC_MEASUREMENT = locale_extra;
-    LC_MONETARY = locale_extra;
-    LC_NAME = locale_extra;
-    LC_NUMERIC = locale_extra;
-    LC_PAPER = locale_extra;
-    LC_TELEPHONE = locale_extra;
-    LC_TIME = locale_extra;
+    LC_ADDRESS = "ru_RU.UTF-8";
+    LC_IDENTIFICATION = "ru_RU.UTF-8";
+    LC_MEASUREMENT = "ru_RU.UTF-8";
+    LC_PAPER = "ru_RU.UTF-8";
+    LC_TELEPHONE = "kk_KZ.UTF-8";
   };
   # WARN: enabling xserver also starts GDM by default — only enable when a desktop is configured
   /*
