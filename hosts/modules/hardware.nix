cb44cbd7ce4362e7fa3a42b65dccafac1cfe48aa
rakhat <yskak.rakhat@gmail.com>
2026-06-26 08:25:28 +0500

    feat: force nvidia

--- hosts/modules/hardware.nix
+++ hosts/modules/hardware.nix
@@ -1,4 +1,10 @@
-{ ... }:
+{ lib, ... }:
 {
   hardware.graphics.enable = true;
+
+  hardware.nvidia.prime = {
+    offload.enable = lib.mkForce false;
+    offload.enableOffloadCmd = lib.mkForce false;
+    sync.enable = lib.mkForce true;
+  };
 }
