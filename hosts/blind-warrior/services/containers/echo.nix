c10b060962e67082fd7168b727f85bfe481f9969
rakhat <yskak.rakhat@gmail.com>
2026-06-26 19:16:00 +0500

    refactor: re-enable containers

--- hosts/blind-warrior/services/containers/echo.nix
+++ hosts/blind-warrior/services/containers/echo.nix
@@ -2,7 +2,7 @@
   virtualisation.oci-containers.containers."echo-http-service" = {
     image = "hashicorp/http-echo:latest";
     extraOptions = [
-      "-text='Hello, World!'"
+      "-text=Hello, World!"
       "--network=web"
     ];
     ports = [ "5678:5678" ];
