3d33112df05bf1af992f4a1a249e3b494ff6917f
rakhat <yskak.rakhat@gmail.com>
2026-06-27 12:39:51 +0500

    feat: prefer dark theme

--- home/features/desktop/default.nix
+++ home/features/desktop/default.nix
@@ -3,4 +3,14 @@
     ./fonts.nix
     ./hyprland
   ];
+
+  gtk = {
+    enable = true;
+    gtk3.extraConfig."gtk-application-prefer-dark-theme" = true;
+  };
+
+  dconf.settings."org/gnome/desktop/interface" = {
+    color-scheme = "prefer-dark";
+    gtk-application-prefer-dark-theme = true;
+  };
 }
