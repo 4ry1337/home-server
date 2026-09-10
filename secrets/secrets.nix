b25062bdf549eca13c6ba43b4918043d96f07278
Rakhat Yskak (ary) <yskak.rakhat@gmail.com>
2026-09-10 19:49:00 +0500

    feat: tailscale added
    Signed-off-by: Rakhat Yskak (ary) <yskak.rakhat@gmail.com>

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,12 @@
 let
-  rakhat = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAw073fJwGbhBTWk6Yy2h/MTGnUJrbjnMQ8hJ0PLZGJr rakhat"
+  rakhat = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAw073fJwGbhBTWk6Yy2h/MTGnUJrbjnMQ8hJ0PLZGJr rakhat";
   ary = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBP/gBUdlpniVJ8JJ1WTFDHDkjvFYBNm0jD4jk2sIZYx root@ary";
   blind-warrior = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOn7sv142ULZQ8ftbPUY2VmopVBAtcBBTKDkSnghpUyO root@rakhat-server";
-  hosts = [ ary ];
-in {
+  hosts = [
+    ary
+    blind-warrior
+  ];
+in
+{
   "tailscale-authkey.age".publicKeys = [ rakhat ] ++ hosts;
 }
