af73a4fcc620c8fe0b805f94f2af4c503cd350ae
Rakhat Yskak (ary) <yskak.rakhat@gmail.com>
2026-09-18 15:07:36 +0500

    fix: devenv nixpkgs unstable fixed
    Signed-off-by: Rakhat Yskak (ary) <yskak.rakhat@gmail.com>

--- home/features/dev/default.nix
+++ home/features/dev/default.nix
@@ -18,7 +18,7 @@ in
     features.dev.ai.claude-code.enable = mkDefault true;
 
     home.packages = with pkgs; [
-      stable.devenv
+      devenv
       gcc
       vulkan-tools # Graphics debugging tools
     ];
